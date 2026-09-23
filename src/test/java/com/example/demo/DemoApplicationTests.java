173303f0cb32f8655ca09d08fdf718a7b37a5f9a
Madhu-gt-hb <madhusmail271@gmail.com>
2026-09-23 21:06:19 +0530

    Replace DemoApplicationTests with HelloController

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,13 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootTest
-class DemoApplicationTests {
+@RestController
+public class HelloController {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @GetMapping("/")
+    public String hello() {
+        return "Hello from Jenkins CI/CD + Tomcat!";
+    }
 }
