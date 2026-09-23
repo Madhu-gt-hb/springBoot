b31aef04662a3260abb5b77b0f6122a1600a2c4d
Madhu-gt-hb <madhusmail271@gmail.com>
2026-09-23 23:58:10 +0530

    Update Demo1ApplicationTests.java

--- src/test/java/com/example/demo1/Demo1ApplicationTests.java
+++ src/test/java/com/example/demo1/Demo1ApplicationTests.java
@@ -1,18 +1,13 @@
 package com.example.demo1;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-<<<<<<< HEAD:src/test/java/com/example/demo1/Demo1ApplicationTests.java
 @SpringBootTest
 class Demo1ApplicationTests {
-=======
-@SpringBootApplication
-public class DemoApplication {
->>>>>>> 321356aef4855aaeeb0a59449376b79f9db049ef:src/test/java/com/example/demo/DemoApplicationTests.java
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
