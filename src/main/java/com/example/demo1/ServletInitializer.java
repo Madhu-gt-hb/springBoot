c7cb68e88c2db7ba585077115f30a18fc3b31803
Gt-hb1 <madhusmail269@gmail.com>
2026-09-23 23:24:17 +0530

    modified all file

--- src/main/java/com/example/demo1/ServletInitializer.java
+++ src/main/java/com/example/demo1/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo1;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(DemoApplication.class);
+		return application.sources(Demo1Application.class);
 	}
 
 }
